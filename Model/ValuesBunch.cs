152c84185535452cb1415d6fd6289b6b4020d2db
Fedor Ishchenko <aleks180700@gmail.com>
2020-06-25 18:32:53 +1000

    Model extension to work with ranges

--- Model/ValuesBunch.cs
+++ Model/ValuesBunch.cs
@@ -4,17 +4,41 @@ using System.Collections.Generic;
 
 namespace Model
 {
+	/// <summary>
+	/// Abstraction for items range for curtain period
+	/// </summary>
 	public partial class ValuesBunch
 	{
-		public DateTime Date { get; }
+		public DateTime InitialDate { get; }
+
+		public DateTime? FinalDate { get; }
 
 		public int Count => values.Count;
 
 		private readonly List<ValueItem> values = new List<ValueItem>();
 
+		/// <summary>
+		/// Creates <see cref="ValuesBunch"/> object from <paramref name="items"/> for single <paramref name="date"/>
+		/// </summary>
+		/// <param name="date"></param>
+		/// <param name="items"></param>
 		public ValuesBunch(DateTime date, List<ValueItem> items)
 		{
-			Date = date;
+			InitialDate = date;
+			FinalDate = null;
+			values = items ?? throw new ArgumentNullException(nameof(items));
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="initialDate"></param>
+		/// <param name="finalDate"></param>
+		/// <param name="items"></param>
+		public ValuesBunch(DateTime initialDate, DateTime finalDate, List<ValueItem> items)
+		{
+			InitialDate = initialDate;
+			FinalDate = finalDate;
 			values = items ?? throw new ArgumentNullException(nameof(items));
 		}
 
@@ -25,6 +49,7 @@ namespace Model
 		}
 
 	}
+
 	//TODO: should I add one more alternative ctor to Scopes, which takes some interfaced item
 	public partial class ValuesBunch : IEnumerable<ValueItem>
 	{
--- Utils/ExcelExportManager.cs
+++ Utils/ExcelExportManager.cs
@@ -36,6 +36,7 @@ namespace Utils
 			var lastColumn = worksheet.LastColumnUsed();
  
 
+
 			return null;
 		}
 	}
