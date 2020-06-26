742adb46ca7556fa39575438818eca326e234f48
Fedor Ishchenko <aleks180700@gmail.com>
2020-06-26 20:13:01 +1000

    PiePiece selection color was changed

--- PieDiagramControls/PiePiece.xaml.cs
+++ PieDiagramControls/PiePiece.xaml.cs
@@ -72,8 +72,7 @@ namespace PieDiagramControls
 		public void Select()
 		{
 			MainPath.StrokeThickness = 3;
-			//#E999FF
-			MainPath.Fill = new SolidColorBrush(Color.FromArgb(255, 233, 153, 255));
+			MainPath.Fill = Brushes.LightGray;
 
 			MouseIn?.Invoke(this);
 		}
